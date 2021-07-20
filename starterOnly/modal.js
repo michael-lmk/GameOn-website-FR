5d162c141b37794d67d9aa2aa737941da4a9a02c
michael-lmk <>
2021-07-20 14:37:12 +0200

    ADD : Close modal button

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,22 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModalBtn = document.querySelector('.close');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// Close modal event
+closeModalBtn.addEventListener("click", closeModal)
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+// close modal form
+function closeModal(){
+    modalbg.style.display = "none";
 }
 
 
